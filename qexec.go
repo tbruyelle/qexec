9a10b651b45fc520cd73106575371e7024702d0a
Thomas Bruyelle <thomas.bruyelle@gmail.com>
2014-06-04 16:07:45 +0200

    Handle common mistake when the executable contains a env var

--- qexec.go
+++ qexec.go
@@ -10,15 +10,44 @@ import (
 
 // Run executes the command in parameter after having correctly quoted it.
 // The command stdout is returned.
+//
+// It handles a common error when the path to the executable contains one or more
+// environment variable, which usually produces an error `no such file
+// or directory`. This is because `os/exec` checks the existence of the
+// executable and it doesn't interpret the environment variables.
+// Here if the executable contains any $ char, then the whole command is
+// wrapped by `sh -c "<command>"`.
 func Run(cmds ...string) (string, error) {
-	toRun := strings.Join(cmds, " ")
-	input, err := shellquote.Split(toRun)
+	if strings.Contains(cmds[0], "$") {
+		// If the path to the executable contains env variables,
+		// then the command must be wrapped by `sh -c "<command>"`
+		wrap := []string{"sh", "-c", `"`}
+		wrap = append(wrap, cmds...)
+		wrap = append(wrap, `"`)
+		cmds = wrap
+	}
+	name, args, err := quote(cmds)
 	if err != nil {
 		return "", err
 	}
-	name := input[0]
-	arg := input[1:]
-	cmd := exec.Command(name, arg...)
+	res, err := run(name, args)
+	if err != nil {
+		return "", err
+	}
+	return res, nil
+}
+
+func quote(cmds []string) (string, []string, error) {
+	toRun := strings.Join(cmds, " ")
+	input, err := shellquote.Split(toRun)
+	if err != nil {
+		return "", nil, err
+	}
+	return input[0], input[1:], nil
+}
+
+func run(name string, args []string) (string, error) {
+	cmd := exec.Command(name, args...)
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	cmd.Stdin = os.Stdin
